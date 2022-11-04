e72ddd8aa08007247f234995e2fa454253a03ef3
Md Rifat Hossen Saown <rifatsaown0@gmail.com>
2022-11-05 02:12:02 +0600

    data loaded succesfully

---
+++ src/components/Shop/Shop.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Products from "../Products/Products";
+import './Shop.css';
+
+const Shop = () => {
+    const [products, setProducts] = React.useState([]);
+    React.useEffect(() => {
+        fetch('./products.json')
+            .then(res => res.json())
+            .then(data => setProducts(data))
+    }, []);
+  return (
+    <div className='shop-container'>
+      <div className="product-container">
+        <h2>Product Loaded : {products.length}</h2>
+        <Products/>
+      </div>
+      <div className="cart-container">
+        <h3>Order Summary</h3>
+        <p>Select Item : </p>
+        <p>Total Price : $</p>
+      </div>
+    </div>
+  );
+};
+
+export default Shop;
