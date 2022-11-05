525cfab18c0d3d5463bcf0603a32d266a3328f07
Md Rifat Hossen Saown <rifatsaown0@gmail.com>
2022-11-05 23:08:01 +0600

    css added shop

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,17 +1,22 @@
 import React from "react";
-import './Product.css';
+import "./Product.css";
 
 const Products = (props) => {
   const { name, img, seller, price, ratings } = props;
   return (
     <div className="product">
       <img src={img} alt="" />
-      <p className="product-name">{name}</p>
-      <p>Price : ${price}</p>
-      <small>
-      <p>By : {seller}</p>
-      <p>Rating : {ratings}</p>
-      </small>
+      <div className="product-info">
+        <p className="product-name">{name}</p>
+        <p>Price : ${price}</p>
+        <p>
+          <small>By : {seller}</small>
+        </p>
+        <p>
+          <small>Rating : {ratings} Stars</small>
+        </p>
+      </div>
+      <button className="btn-cart">Add to Cart</button>
     </div>
   );
 };
