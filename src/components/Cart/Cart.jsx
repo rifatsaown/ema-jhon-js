f2276ecf6c75c905f9794df4b8b25fcf7e8be2c2
Md Rifat Hossen Saown <rifatsaown0@gmail.com>
2022-11-10 01:40:28 +0600

    localstorage work

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,11 +1,23 @@
 import React from "react";
+import "./Cart.css";
 
 export default function Cart({ cart }) {
+    let shipping = 0;
+    let total = 0;
+    for (const product of cart) {
+        total = total + product.price;
+        shipping = shipping + product.shipping;
+    }
+    let tax = Math.ceil((total + shipping) * 0.1);
+    let grandTotal = total + shipping + tax;
   return (
-    <div>
-      <h3>Order Summary</h3>
+    <div className="cart">
+      <h2>Order Summary</h2>
       <p>Select Item : {cart.length}</p>
-      <p>Total Price : $</p>
+      <p>Total Price : ${total}</p>
+      <p>Shipping Charge : ${shipping}</p>
+      <p>Tax : ${tax}</p>
+      <h4>Grand Total : ${grandTotal}</h4>
     </div>
   );
 }
