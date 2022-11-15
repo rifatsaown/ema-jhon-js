11f4d0c1ae1efac5200c2aada3a94340ea8ae01a
Md Rifat Hossen Saown <rifatsaown0@gmail.com>
2022-11-15 23:37:23 +0600

    stord cart update

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import "./Cart.css";
 
 export default function Cart({ cart }) {
-    let shipping = 0;
-    let total = 0;
-    for (const product of cart) {
-        total = total + product.price;
-        shipping = shipping + product.shipping;
-    }
-    let tax = Math.ceil((total + shipping) * 0.1);
-    let grandTotal = total + shipping + tax;
-    
+  let shipping = 0;
+  let total = 0;
+  for (const product of cart) {
+    total = total + product.price;
+    shipping = shipping + product.shipping;
+  }
+  let tax = Math.ceil((total + shipping) * 0.1);
+  let grandTotal = total + shipping + tax;
+
   return (
     <div className="cart">
       <h2>Order Summary</h2>
